2804798b37bcf28dc1daa733ad34e8630c5c3382
John George <john@exotel.in>
2019-04-29 15:49:40 +0530

    added api to get all wines

--- drunkenMonk/deets/deets.go
+++ drunkenMonk/deets/deets.go
@@ -9,7 +9,7 @@ import (
 
 func GetAllCountries() []string {
 	var country string
-	countryList := make([]string, 0)
+	countryList := make([]string, 0, 100)
 	db, err := sql.Open("mysql", "root:password@/winereviews?multiStatements=true")
 	defer db.Close()
 	checkError(err)
@@ -20,11 +20,29 @@ func GetAllCountries() []string {
 	defer rows.Close()
 	for rows.Next() {
 		err = rows.Scan(&country)
-		fmt.Println(country)
-		countryList := append(countryList, country)
+		countryList = append(countryList, country)
 		fmt.Println(countryList)
 	}
 	return countryList
+}
+
+func GetAllWines() []string {
+	var wine string
+	wineList := make([]string, 0)
+	db, err := sql.Open("mysql", "root:password@/winereviews?multiStatements=true")
+	defer db.Close()
+	checkError(err)
+	stmt, err := db.Prepare("SELECT DISTINCT title from wineDetails")
+	checkError(err)
+	rows, err := stmt.Query()
+	defer rows.Close()
+	return wineList
+	for rows.Next() {
+		err = rows.Scan(&wine)
+		wineList = append(wineList, wine)
+		fmt.Println(wineList)
+	}
+	return wineList
 }
 
 func checkError(err error) {
--- drunkenMonk/main.go
+++ drunkenMonk/main.go
@@ -1,14 +1,23 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
+	"github.com/JohnGeorge47/winereviewer/drunkenMonk/deets"
 	"github.com/labstack/echo"
 )
 
 type User struct {
 	Name  string `json:"name" xml:"name"`
 	Email string `json:"email" xml:"email"`
+}
+type Countries struct {
+	Data []string `json:data`
+}
+
+type Wines struct {
+	Data []string `json:data`
 }
 
 func main() {
@@ -17,13 +26,23 @@ func main() {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
 	e.GET("/countries", getCountries)
+	e.GET("/wines", getAllWines)
 	e.Logger.Fatal(e.Start(":1323"))
 }
 
 func getCountries(c echo.Context) error {
-	u := &User{
-		Name:  "Jon",
-		Email: "jon@labstack.com",
+	countries := deets.GetAllCountries()
+	fmt.Println(countries)
+	u := &Countries{
+		Data: countries,
+	}
+	return c.JSON(http.StatusOK, u)
+}
+
+func getAllWines(c echo.Context) error {
+	wines := deets.GetAllWines()
+	u := &Wines{
+		Data: wines,
 	}
 	return c.JSON(http.StatusOK, u)
 }
